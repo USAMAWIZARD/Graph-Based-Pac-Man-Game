a04086a75d8f1276dcb12faf7fec4ea57fe72f06
USAMAWIZARD <usamawizard@gmail.com>
2022-08-04 16:43:59 +0530

    Dijktra's shortest path algorithm working

--- views/Path Finding Algorithms/Dijkstra's_shortest_path.js
+++ views/Path Finding Algorithms/Dijkstra's_shortest_path.js
@@ -1,26 +1,31 @@
-//
-//          A----3----B---1--E
-//           \      /   \    \        
-//            1    7      5   7       
-//             \  /         \ \         
-//              C ----2------ D  
 
-//graph={'A':{'B':3,'C':1},'B':{'C':7,'E':1,'A':3},'C':{'B':7,'D':2,'A':1},'D':{'B':5,'C':2,'E':7},'E':{'B':1,'D':7}}
-Start_vertex='A'
+                          //          A----1----B---3--E----3----L
+                          //           \      /   \    \        
+                          //            9    2      1   7       
+                          //             \  /         \ \         
+                          //              C ----1------ D  
+
+require('../Path Finding Algorithms/pacmangraph.js')
+//graph={'A':{'B':1,'C':9},'B':{'C':2,'E':3,'A':1,'D':1},'C':{'B':2,'D':1,'A':9},'D':{'B':1,'C':1,'E':7},'E':{'B':3,'D':7,'L':3},'L':{'E':3}}
+function init(pac_position){
+console.log(graph)
+Start_vertex=pac_position
 visited=[Start_vertex]
 shortest_path={}
 shortest_path_way={}
 Object.keys(graph).forEach(vertex => {
   shortest_path[vertex]=Infinity
 });
-
 shortest_path[Start_vertex]=0
+}
 
 function shortest_algo(vertex,cost){
   diff = Object.keys(graph).filter(function(x) { return visited.indexOf(x) < 0 })
   if(diff.length==0){console.log("all visited");return}
-  neighbours=Object.keys(graph[vertex])
+  neighbours= Object.keys(graph[vertex]).filter(function(x) { return visited.indexOf(x) < 0 })
+  if(neighbours.length==0){return}
   current_cost=Infinity
+  console.log("neighbours",neighbours)
   neighbours.forEach(neighbour=>{
     if (!visited.includes(neighbour)){
     console.log("c",graph[vertex][neighbour],neighbour,visited)
@@ -34,10 +39,20 @@ function shortest_algo(vertex,cost){
       }
     }
   })
-  console.log(shortest_path,current_vertex)
-
+  //console.log(shortest_path,current_vertex)
+  console.log("visited",current_vertex)
   visited.push(current_vertex)
- shortest_algo(current_vertex,  shortest_path[current_vertex]  )
+ shortest_algo(current_vertex,  shortest_path[current_vertex] )
+ neighbours= Object.keys(graph[vertex]).filter(function(x) { return visited.indexOf(x) < 0 })
+ console.log("backtrack",neighbours.length,vertex)
+ if(neighbours.length!=0){
+  neighbours.forEach(neighbour=>{
+  shortest_algo(neighbour,  shortest_path[neighbour] )
+  })
+ }
+  else
+  return
 }
-//shortest_algo(Start_vertex,shortest_path[Start_vertex])
-//console.log(shortest_path_way)
+init('0')
+shortest_algo(Start_vertex,shortest_path[Start_vertex])
+console.log("shortest :",shortest_path_way)
